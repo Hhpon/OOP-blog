19dccb87844e132c46c7dd98ab85b9f15590dee7
hhp <624791164@qq.com>
2020-04-08 21:36:44 +0800

    新增mon富文本上传功能

--- admin/src/views/article/edit.vue
+++ admin/src/views/article/edit.vue
@@ -38,7 +38,13 @@
           </el-upload>
         </el-form-item>
         <el-form-item label="文章详情">
-          <mavon-editor v-model="model.body" ref="md" :ishljs="true"></mavon-editor>
+          <mavon-editor
+            v-model="model.body"
+            ref="md"
+            @imgAdd="$imgAdd"
+            @imgDel="$imgDel"
+            :ishljs="true"
+          ></mavon-editor>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" native-type="submit">保存</el-button>
@@ -49,6 +55,7 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   props: {
     id: {}
@@ -56,34 +63,52 @@ export default {
   data() {
     return {
       model: {},
-      categories: []
-    }
+      categories: [],
+      img_file: []
+    };
   },
   methods: {
     async save() {
       if (this.id) {
-        const res = await this.$http.put(`article/${this.id}`, this.model)
+        const res = await this.$http.put(`article/${this.id}`, this.model);
       } else {
-        const res = await this.$http.post('article', this.model)
+        const res = await this.$http.post("article", this.model);
       }
-      this.$router.push('/articles/list')
+      this.$router.push("/articles/list");
       this.$message({
-        type: 'success',
-        message: '保存成功'
-      })
+        type: "success",
+        message: "保存成功"
+      });
     },
     async fetch() {
-      const item = await this.$http.get(`article/${this.id}`)
-      this.model = item.data
+      const item = await this.$http.get(`article/${this.id}`);
+      this.model = item.data;
     },
     async fetchArticle() {
-      const res = await this.$http.get(`category`)
-      this.categories = res.data
+      const res = await this.$http.get(`category`);
+      this.categories = res.data;
+    },
+    // 绑定@imgAdd event
+    $imgAdd(pos, file) {
+      // 第一步.将图片上传到服务器.
+      const formdata = new FormData();
+      formdata.append("file", file);
+      this.img_file[pos] = file;
+      axios
+        .post("http://localhost:3001/api/admin/rest/upload", formdata)
+        .then(res => {
+          let _res = res.data;
+          // 第二步.将返回的url替换到文本原位置![...](0) -> ![...](url)
+          this.$refs.md.$img2Url(pos, _res.url);
+        });
+    },
+    $imgDel(pos) {
+      delete this.img_file[pos];
     }
   },
   created() {
-    this.fetchArticle()
-    this.id && this.fetch()
+    this.fetchArticle();
+    this.id && this.fetch();
   }
-}
+};
 </script>
